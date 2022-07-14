501662fb284e68490ce71088d1f642764771fb78
Theo <theo.acooper@gmail.com>
2022-07-13 22:10:06 -0400

    implemented 3 gfilters

--- src/dsp/node_goertzel.rs
+++ src/dsp/node_goertzel.rs
@@ -1,4 +1,3 @@
-//WRTIEME: convert to node
 // Copyright (c) 2022 theloni-monk <theo.acooper@gmail.com>
 // This file is a part of HexoDSP. Released under GPL-3.0-or-later.
 // See README.md and COPYING for details.
@@ -7,35 +6,40 @@ use crate::nodes::{NodeAudioContext, NodeExecContext};
 use crate::dsp::{NodeId, SAtom, ProcBuf, DspNode, LedPhaseVals, NodeContext};
 use crate::dsp::goertzel::*;
 
-#[macro_export]
-macro_rules! fa_goertzel_type { ($formatter: expr, $v: expr, $denorm_v: expr) => { {
-    let s =
-        match ($v.round() as usize) {
-            0  => "normal",
-            _  => "?",
-        };
-    write!($formatter, "{}", s)
-} } }
-
-
-//WRITME: all of this stuff
-/// A simple amplifier
 #[derive(Debug, Clone)]
-pub struct GzFilt {
-    computer: Goertzel,
+pub struct Gz3Filt {
+    g1: Goertzel,
+    g2: Goertzel,
+    g3: Goertzel,
+
+
+    ofreq1:   f32,
+    ofreq2:   f32,
+    ofreq3:   f32,
+
     srate:   f32,
-    ofreq:   f32,
+    olatency: usize, // how many samples before recomputing goertzel on new window
+    frames_processed: usize,
+
+
     ogain:   f32,
-    otype:   u8,
 }
 
-impl GzFilt {
+impl Gz3Filt {
     pub fn new(_nid: &NodeId) -> Self {
         Self {
-            computer: Goertzel::new(),
-            srate: 1.0 / 44100.0,
-            otype: 99,   // value that can't be set by the user
-            ofreq: -2.0, // value that can't be set by the user
+            g1: Goertzel::new(),
+            g2: Goertzel::new(),
+            g3: Goertzel::new(), 
+
+            ofreq1: 220.0,
+            ofreq2: 330.0,
+            ofreq3: 440.0,
+
+            olatency: 2048,
+            frames_processed: 0,
+
+            srate: 44100.0,
             ogain: -2.0, // value that can't be set by the user
         }
     }
@@ -53,25 +57,29 @@ r#"Goertzel Algorithm
 This is the implementation of a goertzel algorithm for extraction of a particular frequency. It is basically a fine bandpass around a specific frequency.
 "#;
     pub const HELP : &'static str =
-r#"GzFilt - Goertzel Filter (Fine Bandpass)
-
+r#"Gz3Filt - Goertzel Filter (Fine Bandpass)
 This is the implementation of a goertzel algorithm for extraction of a particular frequency. It is basically a fine bandpass around a specific frequency.
+
+It can be used as a frequency follower to extract the amplitudes of 3 different frequencies from a signal.
 "#;
 }
-
-impl DspNode for GzFilt {
+const DEFAULT_BUFFSIZE:usize = 1000; //will get overwritten on first frame anyways
+impl DspNode for Gz3Filt {
     fn outputs() -> usize { 1 }
 
     fn set_sample_rate(&mut self, srate: f32) {
         self.srate = srate;
-        self.computer.srate = srate;
-        self.otype = 99; // cause recalculation of the filter
-
+        self.g1.setCoeff(self.ofreq1, DEFAULT_BUFFSIZE, srate);
+        self.g2.setCoeff(self.ofreq2, DEFAULT_BUFFSIZE, srate);
+        self.g3.setCoeff(self.ofreq3, DEFAULT_BUFFSIZE, srate);
+        
         self.reset();
     }
 
     fn reset(&mut self) {
-        self.computer.reset();
+        self.g1.reset();
+        self.g2.reset();
+        self.g3.reset();
     }
 
     #[inline]
@@ -83,34 +91,68 @@ impl DspNode for GzFilt {
     {
         use crate::dsp::{out, inp, denorm, at};
 
-        let inp   = inp::GzFilt::inp(inputs);
-        let freq  = inp::GzFilt::freq(inputs);
-        let gain  = inp::GzFilt::gain(inputs);
-        let out   = out::GzFilt::sig(outputs);
+        let inp   = inp::Gz3Filt::inp(inputs);
 
-        let cfreq = denorm::GzFilt::freq(freq, 0);
-        let cfreq = cfreq.clamp(0.0, 22000.0);
-        let cgain = denorm::GzFilt::gain(gain, 0);
+        let freq1  = inp::Gz3Filt::freq1(inputs);
+        let freq2  = inp::Gz3Filt::freq2(inputs);
+        let freq3  = inp::Gz3Filt::freq3(inputs);
 
-        if  (cfreq - self.ofreq).abs() > 0.0001
-           || (cgain - self.ogain).abs() > 0.0001
+        let gain  = inp::Gz3Filt::gain(inputs);
+        let out   = out::Gz3Filt::sig(outputs);
+
+        //clamping parameters
+        let cfreq1 = denorm::Gz3Filt::freq1(freq1, 0);
+        let cfreq1 = cfreq1.clamp(0.0, 22000.0);
+        let cfreq2 = denorm::Gz3Filt::freq2(freq2, 0);
+        let cfreq2 = cfreq2.clamp(0.0, 22000.0);
+        let cfreq3 = denorm::Gz3Filt::freq3(freq3, 0);
+        let cfreq3 = cfreq3.clamp(0.0, 22000.0);
+        
+        let cgain = denorm::Gz3Filt::gain(gain, 0);
+
+        let paramschanged = 
+        (cfreq1 - self.ofreq1).abs() > 0.0001 
+        || (cfreq2 - self.ofreq2).abs() > 0.0001 
+        || (cfreq3 - self.ofreq3).abs() > 0.0001 
+        ||(cgain - self.ogain).abs() > 0.0001;
+
+        if paramschanged
         {
             // recalculate coeffs of all in the cascade
-            self.computer.target_freq = cfreq;
-            self.computer.reset();
+            self.g1.target_freq = cfreq1;
+            self.ofreq1 = cfreq1;
+            self.g2.target_freq = cfreq2;
+            self.ofreq2 = cfreq2;
+            self.g3.target_freq = cfreq3;
+            self.ofreq3 = cfreq3;
 
-            self.ofreq = cfreq;
+            self.g1.reset();
+            self.g2.reset();
+            self.g3.reset();
+
             self.ogain = cgain; 
         }
 
-        for frame in 0..ctx.nframes() {
-            let gain  = denorm::GzFilt::gain(gain, frame);
+        self.g1.setCoeff(cfreq1, ctx.nframes(), self.srate);
+        self.g2.setCoeff(cfreq2, ctx.nframes(), self.srate);
+        self.g3.setCoeff(cfreq3, ctx.nframes(), self.srate);
+        
+        if self.frames_processed > self.olatency{
+            self.g1.reset();
+            self.g2.reset();
+            self.g3.reset();
+        }
+
+        for frame in 0..ctx.nframes() { //TODO: update coeff based on nframes
+            let gain  = denorm::Gz3Filt::gain(gain, frame);
 
             let mut s = inp.read(frame);
-            s = self.computer.tick(s);
+            s = self.g1.tick(s) + self.g2.tick(s) + self.g3.tick(s);
 
             out.write(frame, s * gain);
         }
+
+        self.frames_processed+=ctx.nframes();
 
         ctx_vals[0].set(out.read(ctx.nframes() - 1));
     }
