5f82c8b4812f7610efc81111a2ae803c4fd2d362
Theo <theo.acooper@gmail.com>
2022-07-13 21:35:53 -0400

    eliminated private buffer

--- src/dsp/goertzel.rs
+++ src/dsp/goertzel.rs
@@ -1,74 +1,59 @@
-use std::collections::VecDeque;
-
 const PI:f32 = std::f32::consts::PI;
 
 #[derive(Debug, Clone, Default)]
+pub struct GoertzelParams{
+    
+}
+#[derive(Debug, Clone, Default)]
 pub struct Goertzel{
     pub target_freq: f32,
-    pub srate: f32,
-    reference_tuning: f32, // assumed that target freqs will be int multiples of ref tuning
-    ideal_buffsize: usize, // calculated with respect to target freq and ref tuning
-    buff: VecDeque<f32>
-}
+    pub coeff: f32,
 
+    q0: f32,
+    q1: f32,
+    q2: f32,
+}
+const DEFAULT_BUFFSIZE:usize = 100;
 // Calculates an individual term of the Discrete Fourier Series
 // implementation with notes taken from https://www.embedded.com/the-goertzel-algorithm/
 impl Goertzel{
     pub fn new() -> Self {
         let mut s:Goertzel = Default::default();
-        s.srate = 1.0/44100.0;
-        s.target_freq = 880.0;
-        s.reference_tuning = 440.0;
-        s.ideal_buffsize = (2.0 * (1/s.srate) / s.reference_tuning).floor() as usize;
-        s.buff = VecDeque::with_capacity(s.ideal_buffsize);
+        s.setCoeff(880.0, DEFAULT_BUFFSIZE, 44100.0);
+        (s.q0, s.q1, s.q2) = (0.0, 0.0, 0.0);
         s
     }
 
     #[inline]
-    pub fn new_with(tfreq:f32, reftune:f32, srate:f32) -> Self{
+    pub fn new_with(tfreq:f32, buffsize:usize, srate:f32) -> Self{
         let mut s = Self::new();
-        s.target_freq = tfreq;
-        s.reference_tuning = reftune;
-        //you want the target frequencies to be integer multiples of sample_rate/N
-        s.ideal_buffsize = (2.0 * (1.0/srate) / reftune).floor() as usize;
-        s.buff = VecDeque::with_capacity(s.ideal_buffsize);
+        s.setCoeff(tfreq, buffsize, srate);
         s
     }
 
     pub fn reset(&mut self){
-        self.buff.clear();
-        self.ideal_buffsize = (2.0 * (1.0/self.srate) / self.reference_tuning).floor() as usize;
-        self.buff.reserve_exact(self.ideal_buffsize);
+        self.q0 = 0.0;
+        self.q1 = 0.0;
+        self.q2 = 0.0;
+    }
 
+    pub fn setCoeff(&mut self, tfreq:f32, buffsize:usize, srate:f32){
+        self.target_freq = tfreq;
+        let k = (0.5 * ((buffsize as f32 * self.target_freq)/srate as f32)).floor() as f32;
+        let w = (2.0 * PI * k / buffsize as f32) as f32;
+        let c = f32::cos(w);
+        self.coeff = 2.0 * c;
     }
 
     #[inline]
     pub fn tick(&mut self, input: f32) -> f32 {
         let x0 = input;
 
-        // return 0 until the buffer is full
-        if self.buff.len() < self.ideal_buffsize{
-            self.buff.push_back(x0);
-            return 0.0;
-        }
-        self.buff.pop_front();
-        self.buff.push_back(x0);
-        
-        let k = (0.5 * ((self.ideal_buffsize as f32 * self.target_freq)/44100.0)).floor() as f32;
-        let w = (2.0 * PI * k / self.ideal_buffsize as f32) as f32;
-        let c = f32::cos(w);
-        let coeff = 2.0 * c;
+        self.q0 = self.coeff * self.q1 - self.q2 + x0;
+        self.q2 = self.q1;
+        self.q1 = self.q0;
 
-        let mut Q0;
-        let mut Q1 = 0.0;
-        let mut Q2 = 0.0;
-        
-        for i in 0..self.ideal_buffsize{
-            Q0 = coeff* Q1 - Q2 + self.buff.get(i).unwrap();
-            Q2 = Q1;
-            Q1 = Q0;
-        }
-        let mag_squared = (Q1.powf(2.0) + (Q2.powf(2.0)) - (Q1*Q2*coeff)) as f32;
+        let mag_squared = (self.q1.powi(2) + (self.q2.powi(2)) - (self.q1*self.q2*self.coeff)) as f32;
 
         f32::sqrt(mag_squared) / 100.0
     }
