61ae0130641a7dd9579e208d174cf52576b9a1b1
Weird Constructor <weirdconstructor@gmail.com>
2022-10-18 05:05:04 +0200

    Fixed two bugs in the FVaFilt node

--- src/dsp/mod.rs
+++ src/dsp/mod.rs
@@ -1665,14 +1665,14 @@ macro_rules! node_list {
                (2  res   n_id      d_id  r_id   f_def stp_d 0.0, 1.0, 0.5)
                {3 0 ftype setting(8) mode fa_sfilter_type 0 13}
                [0 sig],
-            vfafilt => FVaFilt UIType::Generic UICategory::Signal
+            fvafilt => FVaFilt UIType::Generic UICategory::Signal
                (0  inp   n_id      d_id  r_id   f_def stp_d -1.0, 1.0, 0.0)
                (1 freq  n_pit      d_pit r_fq  f_freq  stp_d -1.0, 0.55064, 1000.0)
                (2  res   n_id      d_id  r_id   f_def stp_d 0.0, 1.0, 0.5)
                (3 drive n_driv     d_driv r_driv f_db  stp_v  0.0, 1.0, 1.0)
-               {4 0 ftype setting(8) mode fa_fvafilt_type 0 2}
-               {5 1 smode setting(8) mode fa_fvafilt_svf_mode 0 4}
-               {6 2 lslope setting(8) mode fa_fvafilt_lslope 0 3}
+               {4 0 ftype setting(0) mode fa_fvafilt_type 0 2}
+               {5 1 smode setting(0) mode fa_fvafilt_svf_mode 0 4}
+               {6 2 lslope setting(0) mode fa_fvafilt_lslope 0 3}
                [0 sig],
             biqfilt => BiqFilt UIType::Generic UICategory::Signal
                (0 inp    n_id      d_id  r_id   f_def stp_d -1.0, 1.0, 0.0)
--- tests/node_fvafilt.rs
+++ tests/node_fvafilt.rs
@@ -6,23 +6,15 @@ mod common;
 use common::*;
 
 fn setup_fvafilt_matrix() -> (Matrix, NodeExecutor) {
-    let (node_conf, node_exec) = new_node_engine();
+    let (node_conf, mut node_exec) = new_node_engine();
     let mut matrix = Matrix::new(node_conf, 3, 3);
 
-    let noise = NodeId::Noise(0);
-    let va = NodeId::FVaFilt(0);
-    let out = NodeId::Out(0);
-    matrix.place(0, 0, Cell::empty(noise).out(None, None, noise.out("sig")));
-    matrix.place(
-        0,
-        1,
-        Cell::empty(va).input(va.inp("inp"), None, None).out(None, None, va.out("sig")),
-    );
-    matrix.place(0, 2, Cell::empty(out).input(out.inp("ch1"), None, None));
-    matrix.place(1, 1, Cell::empty(noise).out(None, None, noise.out("sig")));
-    matrix.place(1, 2, Cell::empty(out).input(out.inp("ch2"), None, None));
-    pset_n(&mut matrix, noise, "atv", 1.0);
-//    pset_d(&mut matrix, va, "freq", 17219.88);
+    let mut chain = MatrixCellChain::new(CellDir::B);
+    chain
+        .node_out("noise", "sig")
+        .node_io("fvafilt", "inp", "sig")
+        .node_inp("out", "ch1")
+        .place(&mut matrix, 0, 0).unwrap();
     matrix.sync().unwrap();
 
     (matrix, node_exec)
@@ -47,16 +39,13 @@ fn check_node_fvafilt_ladder() {
     let (mut matrix, mut node_exec) = setup_fvafilt_matrix();
 
     let va = NodeId::FVaFilt(0);
-//    pset_d(&mut matrix, va, "freq", 19000.0);
-//    pset_d(&mut matrix, va, "res", 0.0);
-//    pset_d_wait(&mut matrix, &mut node_exec, va, "drive", 2.0);
     println!("START");
-
-    let out = run_and_get_fft4096(&mut node_exec, 10, 500.0);
 
     pset_d(&mut matrix, va, "freq", 250.00);
     pset_d(&mut matrix, va, "res", 0.5);
-    pset_d_wait(&mut matrix, &mut node_exec, va, "drive", 2.0);
+    pset_d_wait(&mut matrix, &mut node_exec, va, "drive", 1.0);
+
+    let out = run_and_get_fft4096(&mut node_exec, 2, 500.0);
 
     println!("{:#?}", out);
 //    assert!(false);
