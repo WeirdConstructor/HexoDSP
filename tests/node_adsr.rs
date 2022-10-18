5a83ea295515fd3faa2fb5fb815e25d2e4a71b7d
Weird Constructor <weirdconstructor@gmail.com>
2022-10-18 03:57:16 +0200

    Finished ADSR envelope test

--- tests/node_adsr.rs
+++ tests/node_adsr.rs
@@ -60,64 +60,14 @@ fn check_node_adsr_1() {
         res.0,
         50,
         vec![
-            0.0,
-            0.0,
-            0.0,
-            0.0,
-            0.0,
-            0.0,
-            0.0,
+            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
             // 44.1 per ms, decay is default 40.0ms (roughly 35 * 50 samples):
-            -0.0002834,
-            -0.0002834,
-            -0.0002834,
-            -0.0002834,
-            -0.0002834,
-            -0.0002834,
-            -0.0002834,
-            -0.0002834,
-            -0.0002834,
-            -0.0002834,
-            -0.0002834,
-            -0.0002834,
-            -0.0002834,
-            -0.0002834,
-            -0.0002834,
-            -0.0002834,
-            -0.0002834,
-            -0.0002834,
-            -0.0002834,
-            -0.0002834,
-            -0.0002834,
-            -0.0002834,
-            -0.0002834,
-            -0.0002834,
-            -0.0002834,
-            -0.0002834,
-            -0.0002834,
-            -0.0002834,
-            -0.0002834,
-            -0.0002834,
-            -0.0002834,
-            -0.0002834,
-            -0.0002834,
-            -0.0002834,
-            -0.0002834,
-            0.0,
-            0.0,
-            0.0,
-            0.0,
-            0.0,
-            0.0,
-            0.0,
-            0.0,
-            0.0,
-            0.0,
-            0.0,
-            0.0,
-            0.0,
-            0.0,
-            0.0,
+            -0.0002834, -0.0002834, -0.0002834, -0.0002834, -0.0002834, -0.0002834, -0.0002834,
+            -0.0002834, -0.0002834, -0.0002834, -0.0002834, -0.0002834, -0.0002834, -0.0002834,
+            -0.0002834, -0.0002834, -0.0002834, -0.0002834, -0.0002834, -0.0002834, -0.0002834,
+            -0.0002834, -0.0002834, -0.0002834, -0.0002834, -0.0002834, -0.0002834, -0.0002834,
+            -0.0002834, -0.0002834, -0.0002834, -0.0002834, -0.0002834, -0.0002834, -0.0002834,
+            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
         ]
     );
 }
@@ -136,6 +86,87 @@ fn check_node_adsr_2() {
     node_pset_d(&mut matrix, "adsr", 0, "dcy", 5.0);
     node_pset_d(&mut matrix, "adsr", 0, "rel", 5.0);
     node_pset_d(&mut matrix, "adsr", 0, "sus", 0.2);
+    wait_params_smooth(&mut node_exec);
+    node_pset_n(&mut matrix, "adsr", 0, "gate", 1.0);
+    let res = run_for_ms(&mut node_exec, 20.0);
+    assert_decimated_slope_feq!(
+        res.0,
+        50,
+        vec![
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            // 44.1 per ms, attack is default 3.0ms (roughly 3 * 50 samples):
+            0.0045351535,
+            0.0045351535,
+            0.0045351535,
+            0.0045351535,
+            // 44.1 per ms, decay is default 10.0ms (roughly 9 * 50 samples):
+            -0.0036280751,
+            -0.0036280751,
+            -0.0036280751,
+            -0.0036280751,
+            -0.0036280751,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0
+        ]
+    );
+
+    node_pset_n(&mut matrix, "adsr", 0, "gate", 0.0);
+    let res = run_for_ms(&mut node_exec, 50.0);
+    assert_decimated_slope_feq!(
+        res.0,
+        50,
+        vec![
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            // 44.1 per ms, decay is default 40.0ms (roughly 35 * 50 samples):
+            -0.0009070337,
+            -0.0009070337,
+            -0.0009070337,
+            -0.0009070337,
+            -0.00045377004,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+        ]
+    );
+}
+
+#[test]
+// Tests attack decay and release value adjustment.
+fn check_node_adsr_mult() {
+    let (node_conf, mut node_exec) = new_node_engine();
+    let mut matrix = Matrix::new(node_conf, 3, 3);
+
+    let mut chain = MatrixCellChain::new(CellDir::B);
+    chain.node_out("adsr", "sig").node_inp("out", "ch1").place(&mut matrix, 0, 0).unwrap();
+    matrix.sync().unwrap();
+
+    node_pset_d(&mut matrix, "adsr", 0, "atk", 0.5);
+    node_pset_d(&mut matrix, "adsr", 0, "dcy", 0.5);
+    node_pset_d(&mut matrix, "adsr", 0, "rel", 0.5);
+    node_pset_d(&mut matrix, "adsr", 0, "sus", 0.2);
+    node_pset_s(&mut matrix, "adsr", 0, "mult", 1);
     wait_params_smooth(&mut node_exec);
     node_pset_n(&mut matrix, "adsr", 0, "gate", 1.0);
     let res = run_for_ms(&mut node_exec, 20.0);
@@ -298,6 +329,41 @@ fn check_node_adsr_3() {
             0.0,
             0.0,
             0.0,
+        ]
+    );
+}
+
+#[test]
+// Tests attack decay and release shape adjustment.
+fn check_node_adsr_eoet() {
+    let (node_conf, mut node_exec) = new_node_engine();
+    let mut matrix = Matrix::new(node_conf, 3, 3);
+
+    let mut chain = MatrixCellChain::new(CellDir::B);
+    chain.node_out("adsr", "eoet").node_inp("out", "ch1").place(&mut matrix, 0, 0).unwrap();
+    matrix.sync().unwrap();
+
+    node_pset_d(&mut matrix, "adsr", 0, "atk", 10.0);
+    node_pset_d(&mut matrix, "adsr", 0, "dcy", 10.0);
+    node_pset_d(&mut matrix, "adsr", 0, "rel", 10.0);
+    node_pset_d(&mut matrix, "adsr", 0, "sus", 0.2);
+    wait_params_smooth(&mut node_exec);
+    node_pset_n(&mut matrix, "adsr", 0, "gate", 1.0);
+    let res = run_for_ms(&mut node_exec, 40.0);
+    assert_decimated_feq!(res.0, 50, vec![0.0; 100]);
+
+    node_pset_n(&mut matrix, "adsr", 0, "gate", 0.0);
+    let res = run_for_ms(&mut node_exec, 50.0);
+    assert_decimated_feq!(
+        res.0,
+        50,
+        vec![
+            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+            1.0, // end of envelope trigger!
+            1.0, // end of envelope trigger!
+            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+            0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
         ]
     );
 }
